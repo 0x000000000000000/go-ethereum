81ddcf1253d604b9b24126a8c8082ff5fbef9bc4
陈飞 <chenfei@tianrukj.com>
2023-02-21 10:10:21 +0800

    meger v1.10.26-dev

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -228,6 +228,16 @@ func (ec *Client) TransactionByHash(ctx context.Context, hash common.Hash) (tx *
 	return json.tx, json.BlockNumber == nil, nil
 }
 
+// TransactionByHash returns the transaction with the given hash.
+func (ec *Client) GetBoundTransactionsAndPredictDoCall(ctx context.Context, hash common.Hash, input hexutil.Bytes) ([]*ethapi.RPCTransactionPlus, error) {
+	var json []*ethapi.RPCTransactionPlus
+	err := ec.c.CallContext(ctx, &json, "eth_getBoundTransactionsAndPredictDoCall", &hash, input)
+	if err != nil {
+		return nil, err
+	}
+	return json, nil
+}
+
 // TransactionSender returns the sender address of the given transaction. The transaction
 // must be known to the remote node and included in the blockchain at the given block and
 // index. The sender is the one derived by the protocol at the time of inclusion.
--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1494,6 +1494,7 @@ type TransactionAPI struct {
 	b         Backend
 	nonceLock *AddrLocker
 	signer    types.Signer
+	bc        *BlockChainAPI
 }
 
 // NewTransactionAPI creates a new RPC service with methods for interacting with transactions.
@@ -1501,7 +1502,7 @@ func NewTransactionAPI(b Backend, nonceLock *AddrLocker) *TransactionAPI {
 	// The signer used by the API should always be the 'latest' known one because we expect
 	// signers to be backwards-compatible with old transactions.
 	signer := types.LatestSigner(b.ChainConfig())
-	return &TransactionAPI{b, nonceLock, signer}
+	return &TransactionAPI{b: b, nonceLock: nonceLock, signer: signer}
 }
 
 // GetBlockTransactionCountByNumber returns the number of transactions in the block with the given block number.
